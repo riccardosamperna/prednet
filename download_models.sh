d72d85746e67bfa4c50af65fc991c5b8d4303162
riccardosamperna <riccardosamperna@users.noreply.github.com>
2017-01-08 13:38:13 +0100

    Delete download_models.sh

--- download_models.sh
+++
@@ -1,6 +0,0 @@
-savedir="model_data"
-mkdir -p -- "$savedir"
-wget https://www.dropbox.com/s/n6hllbbaeh3fpj9/prednet_kitti_model.zip?dl=0 -O $savedir/prednet_kitti_model.zip
-unzip $savedir/prednet_kitti_model.zip -d $savedir
-wget https://www.dropbox.com/s/zhcp20ixvufnma8/prednet_kitti_model-extrapfinetuned.zip?dl=0 -O $savedir/prednet_kitti_model-extrapfinetuned.zip
-unzip $savedir/prednet_kitti_model-extrapfinetuned.zip -d $savedir
